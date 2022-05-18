6a6c6f03c4663e1bea5d2570fae27cd2ba9a5fe8
E-K8 <ekatplat@gmail.com>
2022-05-18 22:02:51 +0100

    submit feedback from the form and save it to feedback.json

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
-// import path
+import path from 'path';
 
-// this function is for executing server-side code
+// this function ↓↓↓ is for executing server-side code (and only)
 
 function handler(req, res) {
   if (req.method === 'POST') {
@@ -9,17 +9,21 @@ function handler(req, res) {
     const feedbackText = req.body.text;
 
     const newFeedback = {
-      id: new Data().toISOString,
+      id: new Date().toISOString(),
       email: email,
       text: feedbackText,
     };
     // store that ^^^ in a database or in a file
-    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json'); //will create absolute path to the folder for us
     const fileData = fs.readFileSync(filePath);
     const data = JSON.parse(fileData);
     data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({ message: 'Success!', feedback: newFeedback });
+  } else {
+    res.status(200).json({ message: 'this works!' });
   }
-  res.status(200).json({ message: 'this works!' });
 }
 
 export default handler;
